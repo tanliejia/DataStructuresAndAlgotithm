f5284ddead2ec39e6d7571bbc53885fe6bbc7c90
tanliejia <59403533+tanliejia@users.noreply.github.com>
2020-01-01 18:43:09 +0800

    线性问题
    线性表、栈、队列

---
+++ 线性/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("栈是后进先出队列是先进先出线性表……就是存储并列数据用的呗……栈和队列都可以用线性表表示");
+            Console.WriteLine("排队打饭，符合队列的特征，火车头调头符合栈的特征");
+        }
+    }
+}
